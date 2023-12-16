c3e853a6f045449f66d8708c913a7f7cfe021bf3
Godswill Ezeala <ezealagodswill@gmail.com>
2023-12-16 11:37:20 +0100

    server build

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
 	function middleware(req) {
-		// console.log(req.nextauth.token);
+		console.log(req.nextauth.token);
 	},
 	{
 		callbacks: {
